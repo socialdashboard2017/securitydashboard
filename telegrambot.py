6f89f495da796013f2df15fd52cc726115618e9e
Andrea Fortuna <andrea.fortuna@gmail.com>
2017-11-29 09:29:54 +0000

    BOT subscriptors

--- telegrambot.py
+++ telegrambot.py
@@ -99,6 +99,10 @@ class BotHandler:
                 db.session.commit()
             self.send_message(chatid,"You are subscribed!")           
         if (message == "/unsubscribe"):
+            subscriber = db.session.query(subscribers).filter_by(chat_id=chatid).first()
+            if subscriber:
+                db.session.delete(subscriber)
+                db.session.commit()
             self.send_message(chatid,"You are unsubscribed!")            
         return "done"
     def registerHook(self):
